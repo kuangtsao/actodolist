c2462d188c3078473f01a797b63fdff62ca7055a
Marcus Liu <me@mail.lmarcus.onl>
2022-01-11 01:36:21 +0800

    安裝 mongoose；測試 db 連線

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+// express related variables
 const express = require('express')
 const app = express()
 const port = 3000
 
+// mongo related variable
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/todo-list')
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+// route
 app.get('/',(req, res) => {
   res.send('Hello Express')
 })
