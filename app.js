f2640d72a88afd6c333ae8955623472b20ec5b96
Marcus Liu <me@mail.lmarcus.onl>
2022-01-14 00:30:59 +0800

    feat: standard排版

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ db.once('open', () => {
 const Todo = require('./models/todo')
 
 // route
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 mongoose 的 Model 物件轉換成乾淨的 Javascript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
@@ -44,11 +44,10 @@ app.get('/todos/new', (req, res) => {
 })
 
 app.post('/todos', (req, res) => {
-  const name = req.body.name  // 從 req.body 拿出表單裡的 name 資料
+  const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
   return Todo.create({ name }) // 存入資料庫
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
-
 })
 
 app.get('/todos/:id', (req, res) => {
