515507fbf9e9712e43d967ef4dfafa2039e90739
Marcus Liu <me@mail.lmarcus.onl>
2022-01-17 16:16:56 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ const Todo = require('./models/todo')
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 mongoose 的 Model 物件轉換成乾淨的 Javascript 資料陣列
+    .sort({ _id: 'asc' }) // 根據 _id 做升冪
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
