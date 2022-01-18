49b828b92abeb7aaa339eeea626236006409d6ff
Marcus Liu <me@mail.lmarcus.onl>
2022-01-18 16:42:34 +0800

    fix: seed 檔案無法載入問題

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -2,7 +2,6 @@ const db = require('../../config/mongoose')
 // 載入 todo-list
 const Todo = require('../todo')
 
-
 db.once('open', () => {
   console.log('insert seeds to mongo')
   for (let i = 0; i < 10 ; i++) {
