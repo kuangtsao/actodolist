377bbed5c7df9a516e2a88734c3cf9e3d34b1d8b
Marcus Liu <me@mail.lmarcus.onl>
2022-04-02 02:11:29 +0800

    modify todeSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,11 +1,35 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 // 載入 todo-list
 const Todo = require('../todo')
+const User = require('../user')
 
+const SEED_USER = {
+  name: 'dummy',
+  email: 'dummyt@example.com',
+  password: '12345678'
+}
 db.once('open', () => {
-  console.log('insert seeds to mongo')
-  for (let i = 0; i < 10 ; i++) {
-    Todo.create({ name: `name-${i}` })
-  }
-  console.log('done')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Todo.create({ name: `name-${i}`, userId }))
+      )
+        .then(() => {
+          console.log('done.')
+          process.exit()
+        })
+    })
 })
